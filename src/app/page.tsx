839521be17d65fb76d745a2ae60b2c3adf6f90ef
Yuzuhausagi <91502201+Yuzuhausagi@users.noreply.github.com>
2024-09-21 15:43:30 +0200

    Added some tailwin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,35 @@ import Image from "next/image";
 import PortifolioItem from "@/components/portifolioItem";
 import {NavBar} from "@/components/NavBar";
 import {Header} from "@/components/Header";
+import Observer from "@/components/Observer";
 
 export default function Home() {
     return (
         <main className="flex justify-center bg-slate-950">
             <NavBar/>
             <div className="flex min-h-screen flex-col items-center justify-between p-24 gap-8" style={{width: 1700}}>
-                <section id={"about"} className="flex flex-col gap-2">
+                <section id={"about"} className="flex flex-col gap-2 pt-16 pb-32">
                     <Header>About Me</Header>
-                    <p className="text-3xl font-bold ">
-                        I am a technology enthusiast with a solid educational background and a proficient understanding
-                        of programming. My passion lies in continuously expanding my knowledge base and refining my
-                        skills as a developer, especially within the dynamic technology landscape. Beyond professional
-                        endeavors, I am deeply engaged in cybersecurity and find great satisfaction in mentoring
-                        {/* eslint-disable-next-line react/no-unescaped-entities */}
-                        students within the university's academic fields.
-                    </p>
+                    <Observer>
+                        <p className="text-3xl font-bold">
+                            I am a technology enthusiast with a solid educational background and a proficient understanding
+                            of programming. My passion lies in continuously expanding my knowledge base and refining my
+                            skills as a developer, especially within the dynamic technology landscape. Beyond professional
+                            endeavors, I am deeply engaged in cybersecurity and find great satisfaction in mentoring
+                            {/* eslint-disable-next-line react/no-unescaped-entities */}
+                            students within the university's academic fields.
+                        </p>
+                    </Observer>
                 </section>
-                <section id={"experience"} className="fkex flex-col gap-2">
+                <section id={"experience"} className="flex flex-col gap-2 pt-32 pb-32 text-center">
                     <Header>Experience</Header>
                     <p className="text-3xl font-bold">
-                        In jahren 1500
+                        Raichuu is gei
                     </p>
                 </section>
-                <section id={"projects"} className="flex flex-col gap-2">
+                <section id={"projects"} className="flex flex-col gap-2 pt-32 pb-32">
                     <Header>Projects</Header>
                     <section id={"portifolio"} className="flex flex-wrap gap-3 sm:max-w-full">
-
                         <PortifolioItem topic={"Portifolio"}
                                         description={"A Discord Bot using the Discord API to automate the server management. It can send welcome messages to newly joined users and assign roles based on their emoticon selections in server messages."}
                                         footer={["NextJS", "React", "TypeScript"]}/>
@@ -53,14 +55,27 @@ export default function Home() {
                                         footer={["JavaScript"]}/>
                     </section>
                 </section>
-                <section id={"contact"} className="flex flex-col gap-2">
+                <section id={"contact"} className="flex flex-col gap-2 pt-32 pb-32 fade">
                     <Header>Contact</Header>
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
                     <span>Don't hesitate to reach out to me. I'll get back to you promptly.</span>
                     <div className="flex justify-center">
-                          <a href="mailto:yosafzamir1998@gmail.com"  className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded " style={{ opacity: 1, transform: 'translateY(0px)'}}>
-                          Contact me!
-                    </a>
+                        <a href="mailto:x"
+                           className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded "
+                           style={{opacity: 1, transform: 'translateY(0px)'}}>
+                            Contact me!
+                        </a>
+                    </div>
+                </section>
+                <section className="social-links-section">
+                    <div className="social-links fixed bottom-4 left-4 flex flex-col space-y-4 text-gray-400">
+                        <a href="https://github.com/Yuzuhausagi" target="_blank">
+                            <img src="/icon/github.svg" alt="Github" className="w-10 h-10 hover:scale-110 transform transition-transform duration-300 text-gray-200 opacity-90"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/yosaf-zamir/" target="_blank">
+                            <img src="/icon/linkedin.svg" alt="LinkedIn"
+                                 className="w-10 h-10 hover:scale-110 transform transition-transform duration-300 text-gray-200 opacity-90"/>
+                        </a>
                     </div>
                 </section>
             </div>
